7fd50dcb94f4160d4ef1dfb4b0950a475c468080
mustafazaimoglu <mkuz138@gmail.com>
2021-01-31 13:02:17 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    // IEntity implemente eden class bir veri tabani tablosudur.
+
+    public interface IEntity
+    {
+
+    }
+}
