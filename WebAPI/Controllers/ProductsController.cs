a2e1c4e6ab7e3b3348ed09f3103f3249e7c25ccb
mustafazaimoglu <mkuz138@gmail.com>
2021-03-13 23:52:49 +0300

    13 Mart

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
@@ -30,6 +31,9 @@ namespace WebAPI.Controllers
         {
             // Swagger
             // Dependeny Chain -- 
+
+            Thread.Sleep(500);
+
             var result = _productService.GetAll();
 
             if (result.Success)
@@ -48,6 +52,19 @@ namespace WebAPI.Controllers
             var result = _productService.GetById(id);
 
             if(result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpGet("getallbycategory")]
+        public IActionResult GetAllByCategoryId(int categoryId)
+        {
+            var result = _productService.GetAllByCategoryId(categoryId);
+
+            if (result.Success)
             {
                 return Ok(result);
             }
